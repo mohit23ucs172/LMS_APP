02521dcee6897321dee6f6ab42e70c03bd54aacc
MOHIT TIWARI <163675793+mohit23ucs172@users.noreply.github.com>
2025-06-27 15:05:54 +0530

    feat: enhance course details and enrollment features with new calculations and UI improvements

--- Frontent/client/src/context/AppContext.jsx
+++ Frontent/client/src/context/AppContext.jsx
@@ -1,6 +1,9 @@
 import  {createContext, useEffect, useState} from 'react'
 import { dummyCourses } from '../assets/assets';
 import { useNavigate } from 'react-router-dom';
+import humanizeDuration from 'humanize-duration'
+
+
 
 export const AppContext=createContext();
 
@@ -10,7 +13,7 @@ const currency=import.meta.env.VITE_CURRENCY
 const navigate=useNavigate();
 
 const [allCourses,setAllCourses]=useState([]);
-
+const [enrolledCourses,setEnrolledCourses]=useState(null);
 const [isEducator,setIsEducator]=useState(true);
 
 //Fetch All COurses
@@ -31,9 +34,47 @@ const calculateRating=(course)=>{
   return totalRating/course.courseRatings.length
 }
 
+//function to calculate course chapter time
+const calculateChapterTime=(chapter)=>{
+  let time=0;
+  chapter.chapterContent.map((lecture)=>{
+    time+=lecture.lectureDuration;
+  })
+  return humanizeDuration(time*60*1000,{units:["h","m"]})
+}
+
+//function to calculate course duration
+const calculateCourseDuration=(course)=>{
+  let time=0;
+  course.courseContent.map((chapter)=>chapter.chapterContent.map((lecture)=>time+=lecture.lectureDuration));
+  return humanizeDuration(time*60*1000,{units:["h","m"]})
+
+}
+
+// function to calculate to No of lectures in the course
+
+const calculateNoOfLecture=(course)=>{
+  let totalLectures=0;
+  course.courseContent.forEach(chapter=>{
+  if(Array.isArray(chapter.chapterContent)){
+    totalLectures+=chapter.chapterContent.length;
+  }
+  })
+  return totalLectures;
+}
+
+//fetch user Enrolled courses
+const fetchUserEnrolledCourses=async()=>{
+setEnrolledCourses(dummyCourses);
+}
+
 useEffect(()=>{
 fetchAllCourses()
+fetchUserEnrolledCourses()
 },[])
+
+
+
 const value={
 currency,
 allCourses,
@@ -41,7 +82,14 @@ navigate,
 calculateRating,
 isEducator,
 setIsEducator,
+calculateChapterTime,
+calculateCourseDuration,
+calculateNoOfLecture,
+enrolledCourses,
+setEnrolledCourses,
+fetchUserEnrolledCourses,
 }
+
 
   return (
     <AppContext.Provider value={value}>
