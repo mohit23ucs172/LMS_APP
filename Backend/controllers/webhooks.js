4f0e074f52baeff3dd8865b376ff94b4d10220c0
MOHIT TIWARI <163675793+mohit23ucs172@users.noreply.github.com>
2025-06-29 10:35:27 +0530

    fix: correct typo in imageUrl assignment for user creation in Clerk webhooks

--- Backend/controllers/webhooks.js
+++ Backend/controllers/webhooks.js
@@ -19,7 +19,7 @@ const clerkWebhooks=async(req,res)=>{
           _id:data.id,
           email:data.email_address[0],
           name:data.first_name+" "+data.last_name,
-          imageUrl:data.immage_url,
+          imageUrl:data.image_url,
         }
         await User.create(userData)
         res.json({})
