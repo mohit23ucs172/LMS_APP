3e126ab3585085d7912c25010e15c0929b49da90
MOHIT TIWARI <163675793+mohit23ucs172@users.noreply.github.com>
2025-06-29 10:36:34 +0530

    fix: correct typo in imageUrl assignment in user.updated case for Clerk webhooks

--- Backend/controllers/webhooks.js
+++ Backend/controllers/webhooks.js
@@ -17,7 +17,7 @@ const clerkWebhooks=async(req,res)=>{
       case 'user.created':{
         const userData={
           _id:data.id,
-          email:data.email_address[0],
+          email:data.email_addresses[0].email_address,
           name:data.first_name+" "+data.last_name,
           imageUrl:data.image_url,
         }
@@ -29,7 +29,7 @@ const clerkWebhooks=async(req,res)=>{
          const userData={
           email:data.email_addresses[0].email_address,
           name:data.first_name+" "+data.last_name,
-          imageUrl:data.immage_url,
+          imageUrl:data.image_url,
         }
         await User.findByIdAndUpdate(data.id,userData)
         res.json({})
